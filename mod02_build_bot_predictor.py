87ee298600a56f27e84d5b44688734aa320f010d
whaledolphinshark <hongyilu123456@gmail.com>
2026-09-16 16:52:38 -0400

    hw 2

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -11,10 +11,10 @@ def train_model(X, y, seed=seed):
     """
     model = GradientBoostingClassifier(
         learning_rate=0.025,
-        n_estimators=200,
+        n_estimators=215,
         max_depth=3,
-        subsample=.9,
-        min_samples_leaf=5,
+        subsample=.85,
+        min_samples_leaf=2,
         random_state=seed
     )
     model.fit(X, y)
