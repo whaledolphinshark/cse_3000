4328140b1465a28338b980ee33c4c74c6237febd
hongyi_lu <hongyi.lu@uconn.edu>
2026-09-16 14:01:03 -0400

    hw 2

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,11 +10,11 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100,
-        max_depth=8,
-        subsample=1,
-        min_samples_leaf=1,
+        learning_rate=0.025,
+        n_estimators=200,
+        max_depth=3,
+        subsample=.9,
+        min_samples_leaf=5,
         random_state=seed
     )
     model.fit(X, y)
